a521bbb32105160b93aef8264d5f2aee9e89f542
Atwaa <mohmedtamer9887@gmail.com>
2026-02-18 18:55:24 +0200

    intro validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,10 +1,34 @@
 <?php
+$config = require("config.php");
+$statment = new db($config['database']);
+
+
+
 $haeding = "Create Note";
 
-
-
+$body = "";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
+
+    $errors = [];
+    $body = $_POST['body'];
+
+    // check if body is less than 3 and greater than 255 characters
+    if (strlen(trim($body)) > 255 || strlen(trim($body)) < 3) {
+        $errors['body'] = "Description must be greater then 3 and  less than 255 characters";
+    }
+
+    // check if body is empty
+    if (strlen(trim($body)) === 0) {
+        $errors['body'] = "Description is required";
+    }
+
+    //  inser into database
+    if (empty($errors)) {
+        $statment->query("INSERT INTO notes (body, user_id) VALUES (:body, :user_id)", [
+            'body' => $body,
+            'user_id' => 1
+        ]);
+    }
 }
 
 require "views/note-create.view.php";
--- views/notes.view.php
+++ views/notes.view.php
@@ -14,7 +14,7 @@
             <?php
             foreach ($notes as $note) {
             ?>
-                <li><a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline"><?= $note['body'] ?></a></li>
+                <li><a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline"><?= htmlspecialchars($note['body']) ?></a></li>
             <?php
             }
             ?>
