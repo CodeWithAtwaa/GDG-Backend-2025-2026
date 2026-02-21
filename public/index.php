5e6896ea0417e300304e11cbbb72d7a6a8bd5c22
Atwaa <mohmedtamer9887@gmail.com>
2026-02-21 02:10:26 +0200

    add pest tst

--- public/index.php
+++ public/index.php
@@ -7,16 +7,11 @@ use Core\ValidationException;
 
 const BASE_PATH = __DIR__ . "/../";
 
+require BASE_PATH . "vendor/autoload.php";
 
 
 require BASE_PATH . "Core/helper.php";
 
-spl_autoload_register(function ($class) {
-    $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
-    require base_path("{$class}.php");
-});
-
-// require(base_path("Core/Router.php"));
 
 require base_path("bootstrap.php");
 
